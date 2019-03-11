06eae2842d72a41279f2434d05f8da5930b00c85
黄俊杰 <huangjunjie@lanyife.com>
2019-03-11 11:25:36 +0800

    增加阿里云内部消息队列处理

---
+++ H.Framework.Aliyun.MNS/MNSCenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace H.Framework.Aliyun.MNS
+{
+    public sealed class MNSCenter
+    {
+        private static readonly Lazy<MNSCenter> _lazy = new Lazy<MNSCenter>(() => new MNSCenter());
+        public static MNSCenter Instance => _lazy.Value;
+        private static List<MNSCore> _coreDic;
+        private readonly object _locker = new object();
+
+        private MNSCenter()
+        {
+            _coreDic = new List<MNSCore>();
+        }
+
+        public MNSCore CoreInstance(string queueName, string accessKeyId = "", string secretAccessKey = "", string endpoint = "")
+        {
+            MNSCore item = null;
+            lock (_locker)
+            {
+                item = _coreDic.FirstOrDefault(x => x.QueueName == queueName);
+                if (item == null)
+                {
+                    item = new MNSCore(queueName, accessKeyId, secretAccessKey, endpoint);
+                    _coreDic.Add(item);
+                }
+                return item;
+            }
+        }
+    }
+}
---
+++ H.Framework.Aliyun.MNS/MNSCore.cs
@@ -0,0 +1,105 @@
+﻿using Aliyun.MNS;
+using Aliyun.MNS.Model;
+using System;
+
+namespace H.Framework.Aliyun.MNS
+{
+    public class MNSCore
+    {
+        private readonly IMNS _client;
+        private AsyncCallback _receiveMessageCallback;
+
+        public MNSCore(string queueName, string accessKeyId, string secretAccessKey, string endpoint)
+        {
+            _client = new MNSClient(accessKeyId, secretAccessKey, endpoint);
+            QueueName = queueName;
+        }
+
+        public string QueueName { get; private set; }
+
+        public Queue Queue => _client.GetNativeQueue(QueueName);
+
+        public SendMessageResponse Send(string messageContent)
+        {
+            try
+            {
+                return Queue.SendMessage(new SendMessageRequest(messageContent));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AsyncSend message failed, exception info: " + ex.Message);
+                return null;
+            }
+        }
+
+        public void SendAsync(string messageContent, AsyncCallback sendMessageCallback)
+        {
+            try
+            {
+                var sendMessageRequest = new SendMessageRequest(messageContent);
+                Queue.BeginSendMessage(sendMessageRequest, sendMessageCallback, Queue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AsyncSend message failed, exception info: " + ex.Message);
+            }
+        }
+
+        public void RegReceiveMessageCallback(AsyncCallback receiveMessageCallback)
+        {
+            _receiveMessageCallback += receiveMessageCallback;
+        }
+
+        public Message Receive()
+        {
+            try
+            {
+                return Queue.ReceiveMessage(30).Message;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AsyncReceive message failed, exception info: " + ex.Message);
+                return null;
+            }
+        }
+
+        public void ReceiveAsync()
+        {
+            try
+            {
+                Queue.BeginReceiveMessage(new ReceiveMessageRequest(), _receiveMessageCallback, Queue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AsyncReceive message failed, exception info: " + ex.Message);
+            }
+        }
+
+        public DeleteMessageResponse Delete(string receiptHandle)
+        {
+            try
+            {
+                var deleteMessageRequest = new DeleteMessageRequest(receiptHandle);
+                return Queue.DeleteMessage(deleteMessageRequest);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AsyncDeleteMessage failed, exception info: " + ex.Message);
+                return null;
+            }
+        }
+
+        public void DeleteAsync(string receiptHandle, AsyncCallback deleteMessageCallback)
+        {
+            try
+            {
+                var deleteMessageRequest = new DeleteMessageRequest(receiptHandle);
+                Queue.BeginDeleteMessage(deleteMessageRequest, deleteMessageCallback, Queue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AsyncDeleteMessage failed, exception info: " + ex.Message);
+            }
+        }
+    }
+}
