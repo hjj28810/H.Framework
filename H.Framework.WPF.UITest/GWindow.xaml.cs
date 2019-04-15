94c241f0060863c36cf7f7f4998942e35f549fd3
黄俊杰 <huangjunjie@lanyife.com>
2019-04-15 18:10:03 +0800

    增加生成防重放

---
+++ H.Framework.WPF.UITest/GWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using H.Framework.Core.Utilities;
+using System;
+using System.Windows;
+
+namespace H.Framework.WPF.UITest
+{
+    /// <summary>
+    /// GWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class GWindow : Window
+    {
+        public GWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var nonce = Utility.ObjectID;
+            var curTime = TimeHelper.UTCSeconds(DateTime.Now).ToString();
+            txt1.Text = nonce;
+            txt2.Text = curTime;
+            txt3.Text = HashEncryptHepler.SHA1Hash(txt0.Text + nonce + curTime).ToLower();
+        }
+    }
+}
