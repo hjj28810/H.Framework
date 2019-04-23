7755195ed71d55eb8fd118d72649fd12cf25b4ab
黄俊杰 <huangjunjie@lanyife.com>
2019-04-23 18:15:57 +0800

    添加阿里云log项目

---
+++ H.Framework.Aliyun.Log/LogCenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace H.Framework.Aliyun.Log
+{
+    public sealed class LogCenter
+    {
+        private static readonly Lazy<LogCenter> _lazy = new Lazy<LogCenter>(() => new LogCenter());
+        public static LogCenter Instance => _lazy.Value;
+        private static List<LogCore> _coreDic;
+        private readonly object _locker = new object();
+
+        private LogCenter()
+        {
+            _coreDic = new List<LogCore>();
+        }
+
+        public LogCore CoreInstance(string projectName, string accessKeyId = "", string secretAccessKey = "", string endpoint = "")
+        {
+            LogCore item = null;
+            lock (_locker)
+            {
+                item = _coreDic.FirstOrDefault(x => x.ProjectName == projectName);
+                if (item == null)
+                {
+                    item = new LogCore(projectName, accessKeyId, secretAccessKey, endpoint);
+                    _coreDic.Add(item);
+                }
+                return item;
+            }
+        }
+    }
+}
---
+++ H.Framework.Aliyun.Log/LogCore.cs
@@ -0,0 +1,20 @@
+﻿using Aliyun.Api.LogService;
+
+namespace H.Framework.Aliyun.Log
+{
+    public class LogCore
+    {
+        public LogCore(string projectName, string accessKeyId, string secretAccessKey, string endpoint)
+        {
+            Client = LogServiceClientBuilders.HttpBuilder
+                           .Endpoint(endpoint, projectName)
+                           .Credential(accessKeyId, secretAccessKey)
+                           .Build();
+            ProjectName = projectName;
+        }
+
+        public ILogServiceClient Client { get; }
+
+        public string ProjectName { get; private set; }
+    }
+}
