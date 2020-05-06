198cbc86a1737f0c2e342303cb4db16d7d285726
Alvin Huang <huangjunjie@lanyife.com>
2020-05-06 13:02:21 +0800

    增加身份证校验

--- H.Framework.Core/Utilities/Extensions.cs
+++ H.Framework.Core/Utilities/Extensions.cs
@@ -837,5 +837,85 @@ namespace H.Framework.Core.Utilities
                    typeof(CategoryAttribute), false);
             return (attributes.Length > 0) ? attributes[0].Category : value.ToString();
         }
+
+        /// <summary>
+        /// 验证身份证合理性
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public static bool CheckIDCard(this string idNumber)
+        {
+            if (idNumber.Length == 18)
+            {
+                bool check = idNumber.CheckIDCard18();
+                return check;
+            }
+            else if (idNumber.Length == 15)
+            {
+                bool check = idNumber.CheckIDCard15();
+                return check;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 18位身份证号码验证
+        /// </summary>
+        private static bool CheckIDCard18(this string idNumber)
+        {
+            if (!long.TryParse(idNumber.Remove(17), out long n) || n < Math.Pow(10, 16) || !long.TryParse(idNumber.Replace('x', '0').Replace('X', '0'), out _))
+            {
+                return false;//数字验证
+            }
+            var address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+            if (address.IndexOf(idNumber.Remove(2)) == -1)
+            {
+                return false;//省份验证
+            }
+            var birth = idNumber.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+            if (DateTime.TryParse(birth, out _) == false)
+            {
+                return false;//生日验证
+            }
+            var arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+            var Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+            var Ai = idNumber.Remove(17).ToCharArray();
+            var sum = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+            }
+            Math.DivRem(sum, 11, out int y);
+            if (arrVarifyCode[y] != idNumber.Substring(17, 1).ToLower())
+            {
+                return false;//校验码验证
+            }
+            return true;//符合GB11643-1999标准
+        }
+
+        /// <summary>
+        /// 15位身份证号码验证
+        /// </summary>
+        private static bool CheckIDCard15(this string idNumber)
+        {
+            if (long.TryParse(idNumber, out long n) == false || n < Math.Pow(10, 14))
+            {
+                return false;//数字验证
+            }
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+            if (address.IndexOf(idNumber.Remove(2)) == -1)
+            {
+                return false;//省份验证
+            }
+            string birth = idNumber.Substring(6, 6).Insert(4, "-").Insert(2, "-");
+            if (DateTime.TryParse(birth, out _) == false)
+            {
+                return false;//生日验证
+            }
+            return true;
+        }
     }
 }
