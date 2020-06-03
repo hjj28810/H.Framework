aca79b3844da4a2a6dd6b6df4b16e6a1445f9837
Alvin Huang <huangjunjie@lanyife.com>
2020-06-03 13:03:43 +0800

    去掉expanderex的headerheight

--- H.Framework.WPF.Control/Controls/ExpanderEx.cs
+++ H.Framework.WPF.Control/Controls/ExpanderEx.cs
@@ -17,17 +17,17 @@ namespace H.Framework.WPF.Control.Controls
             ClipToBoundsProperty.OverrideMetadata(typeof(ExpanderEx), new FrameworkPropertyMetadata(false));
         }
 
-        public static readonly DependencyProperty HeaderHeightProperty = DependencyProperty.Register("HeaderHeight", typeof(double), typeof(ExpanderEx), new PropertyMetadata(12.0, null));
+        //public static readonly DependencyProperty HeaderHeightProperty = DependencyProperty.Register("HeaderHeight", typeof(double), typeof(ExpanderEx), new PropertyMetadata(12.0, null));
 
-        /// <summary>
-        /// Header高度
-        /// </summary>
-        [Description("获取或设置Header高度")]
-        [Category("Defined Properties")]
-        public double HeaderHeight
-        {
-            get => (double)GetValue(HeaderHeightProperty);
-            set => SetValue(HeaderHeightProperty, value);
-        }
+        ///// <summary>
+        ///// Header高度
+        ///// </summary>
+        //[Description("获取或设置Header高度")]
+        //[Category("Defined Properties")]
+        //public double HeaderHeight
+        //{
+        //    get => (double)GetValue(HeaderHeightProperty);
+        //    set => SetValue(HeaderHeightProperty, value);
+        //}
     }
 }
