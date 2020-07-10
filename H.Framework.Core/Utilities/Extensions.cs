b9480d34983abce95f7e316b231b9e1db24d7bcf
Alvin Huang <huangjunjie@lanyife.com>
2020-07-10 10:57:23 +0800

    提交合并

--- H.Framework.Core/Utilities/Extensions.cs
+++ H.Framework.Core/Utilities/Extensions.cs
@@ -824,7 +824,7 @@ namespace H.Framework.Core.Utilities
         {
             var fi = value.GetType().GetField(value.ToString());
             var attributes =
-                   (DescriptionAttribute[])fi.GetCustomAttributes(
+                   (DescriptionAttribute[])fi?.GetCustomAttributes(
                    typeof(DescriptionAttribute), false);
             return (attributes.Length > 0) ? attributes[0].Description : value.ToString();
         }
